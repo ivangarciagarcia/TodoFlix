659ebf2f8ddb9213d963605e406110fce66e566a
Iván García García <garciaivan200@gmail.com>
2023-02-14 18:41:44 +0100

    Seteada la cookie idiomas y bugs
    bugs = Corrección de direcciones de enlaces

--- Imagenes y scripts/index/cookie.js
+++ Imagenes y scripts/index/cookie.js
@@ -1,2 +1,22 @@
+// Comprobar el valor de la cookie de idioma y actualizar el texto del enlace
+function actualizarEnlaceIdioma() {
+  var idioma = localStorage.getItem("idioma");
+  if (idioma === "EN") {
+    document.getElementById("cambiarIdioma").querySelector("a").textContent = "EN";
+  } else {
+    document.getElementById("cambiarIdioma").querySelector("a").textContent = "ES";
+  }
+}
 
+// Actualizar el enlace cuando se carga la página
+document.addEventListener("DOMContentLoaded", actualizarEnlaceIdioma);
 
+// Actualizar el enlace cuando se cambia la cookie de idioma
+document.getElementById("cambiarIdioma").addEventListener("click", function() {
+  if (localStorage.getItem("idioma") === "ES") {
+    localStorage.setItem("idioma", "EN");
+  } else {
+    localStorage.setItem("idioma", "ES");
+  }
+  actualizarEnlaceIdioma();
+});
